82995dc7aaa2f8541ae05314ff9b6d7e9e029edc
christoffer <cmoc@kth.se>
2015-03-19 03:06:18 +0100

    Added before and after links pluss en massa annat

--- src/message_history_handler.erl
+++ src/message_history_handler.erl
@@ -1,43 +1,78 @@
 -module (message_history_handler).
 -export ([init/2]).
--define(MAX_MESSAGES,20).
+-define(MAX_MESSAGES,30).
 
 init(Req, Opts) ->
-	%{ok, _Tokendata} = auth_ball:authenticate(Req),
-	%Thread = cowboy_req:binding(threadid, Req),
-	%QS = cowboy_req:parse_qs(Req),
-	%FromTo = {proplists:get_value(<<"after">>,QS),proplists:get_value(<<"before">>,QS)},
-	%case FromTo of
-		%Get all messages in thread
-	%	{undefined,undefined} -> 
-	%		StartKey = [Thread],
-	%		EndKey = [Thread,{[]}];
-		%Get all messages after StartKey
-	%	{After,undefined} -> 
-	%		StartKey = [Thread, After],
-	%		EndKey = [Thread,{[]}];%{[]} is larger then any number in couchdb
-		%Get MAX_MESSAGES messages before After and return link to get MAX_MESSAGES more if any
-	%	{undefined,Before} ->
-	%		
-	%	{After,Before} ->
-	%		io:format("Error: Supplying after and before not implemented for Threads path.")
-		%Get (MAX_MESSAGES?) messages before After and return link fore more if any before Before
-		
-	%end,
-	%EndKey = [Thread,{[]}],
-	%JSONData = db_utils:query("/_design/Messages/_view/message_history", StartKey, EndKey),
-	%BodyText = jiffy:encode(JSONData),
-	%Headers = [{<<"Content-Type">>,<<"application/json">>}],
-	%Response = cowboy_req:reply(200,
-	%	Headers,
-	%	BodyText,
-	%	Req
-	%),
-	%{ok, Response, Opts}
-	1.
+	case auth_ball:authenticate(Req) of
+		{ok, _Tokendata} ->
+			Thread = cowboy_req:binding(threadid, Req),
+			QS = cowboy_req:parse_qs(Req),
+			After = proplists:get_value(<<"after">>,QS),
+			Before = proplists:get_value(<<"before">>,QS),
+			
+			StartKey = case After of
+				undefined -> 
+					{"startkey",[Thread]};
+				_After ->
+					{"startkey",[Thread, After]}
+			end,
+			
+			{QueryParams,QueryOpts} = case Before of
+				undefined ->
+					EndKey = {"endkey",[Thread,{[]}]},
+					{[EndKey],[]};
+				_Before -> 
+					EndKey = {"endkey",[Thread,Before]},
+					Limit = {"limit",?MAX_MESSAGES},
+					Decending = {"descending",true},
+					{[StartKey,EndKey,Limit,Decending],[reverse]}
+			end,
 
+			JSONData = db_utils:query("/_design/Messages/_view/message_history",QueryParams,{opts,QueryOpts}),
+			Rows = json_utils:get_field(JSONData,<<"rows">>),
+			
+			{[FirstMessage|_]} = Rows,
+			LastMessage = list:last(Rows),
+					
+			FirstMessageId = json_utils:get_field(<<"_id">>,FirstMessage),
+			LastMessageId = json_utils:get_field(<<"_id">>,LastMessage),			
+			
+			BaseLink = "/threads/" ++ Thread ++ "/messages",
+			GotMaxMessages = length(json_utils:get_field(JSONData,<<"rows">>)) == ?MAX_MESSAGES,
+			
+			Links = 
+				if GotMaxMessages ->
+					case {After,Before} of
+						{undefined,undefined} -> 
+							none;
+						{After,undefined} ->
+							BeforeLink = web_utils:create_query_url(BaseLink,[{"before",FirstMessageId}]),
+							{[{<<"before">>,BeforeLink}]};
+						{_,Before} ->
+							BeforeLink = web_utils:create_query_url(BaseLink,[{"before",FirstMessageId}]),
+							AfterLink = web_utils:create_query_url(BaseLink,[{"after",LastMessageId}]),
+							{[{<<"after">>,AfterLink},{<<"before">>,BeforeLink}]}				
+					end;
+				true ->
+					BeforeLink = web_utils:create_query_url(BaseLink,[{"before",FirstMessageId}]),
+					AfterLink = web_utils:create_query_url(BaseLink,[{"after",LastMessageId}]),
+					{[{<<"after">>,AfterLink},{<<"before">>,BeforeLink}]}	
+				end,
+			
+			JSON = case Links of
+				none ->
+					JSONData;
+				_ ->
+					json_utils:add_fields(JSONData,Links)
+			end,
+			
+			web_utils:respond_success(Req,JSON,Opts);
+		Error ->
+			web_utils:respond_resource_not_found(Req,Error)
+	end.
 
-
-get_row_value({Obj}) -> 
-	Value = proplists:get_value(<<"value">>, Obj), 
-	Value.
+% links suppplied in different situations
+% {undefined,undefined} -> na
+% {after,undefined} -> before
+% {undefined,before} -> before, after
+% {after,before} -> before, after
--- src/web_utils.erl
+++ src/web_utils.erl
@@ -1,5 +1,5 @@
 -module(web_utils).
--export([respond_resource_not_found/2,respond_success/3]).
+-export([respond_resource_not_found/2,respond_success/3,create_query_url/2]).
 
 respond_resource_not_found(Req,Error) ->
 	io:format("ERROR: ~p", [Error]),
@@ -14,3 +14,11 @@ respond_success(Req,JSONData,Opts) ->
 		Req
 	),
 	{ok, Response, Opts}.
+	
+create_query_url(QueryBase,[{Key0,Param0}|T]) ->
+	Query0 = "?" ++ Key0 ++ "=" ++ binary_to_list(jiffy:encode(Param0)),
+	QueryN = lists:foldl(
+	fun({KeyK,ParamK},QueryK) ->
+		QueryK ++ "&" ++ KeyK ++ "=" ++ binary_to_list(jiffy:encode(ParamK))
+	end,Query0,T),
+	QueryBase ++ QueryN.
