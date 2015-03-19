9150875cf987ae61164e1cab240ba3dd56645988
Viktor Kronvall <viktor.kronvall@gmail.com>
2015-03-19 22:21:43 +0100

    Implement Admin client sending MQTT notifications.
    Now when threads are created all online users will be notified
    that they are now part of this new thread.

--- src/baseball_app.erl
+++ src/baseball_app.erl
@@ -4,17 +4,27 @@
 -export([start/2]).
 -export([stop/1]).
 
+-define(ONE_WEEK, 604800).
 
 start(_Type, _Args) ->
 	%Change the configuration of baseball by changing the tupple bellow
     random:seed(erlang:now()),
     ObjectId = objectid:objectid(),
+    Payload = {[{admin,true}]},
+    {OID, _} = objectid:objectid(),
+    AdminToken = ejwt:jwt(<<"HS256">>, Payload, ?ONE_WEEK, auth_ball:secret()),
+    mqtt_client_sup:start_link(),
+    MqttParams = [{username, <<"admin">>}, {password, AdminToken}, {client_id, OID}],
+    {ok, MQTTClient} = mqtt_client_simple:connect(MqttParams),
     Dispatch = cowboy_router:compile([
         {'_', [
         	{"/", hello_handler, []},
             {"/threads/:threadid/messages", message_history_handler, []},
         	{"/users/me/threads", my_threads_handler,[]},
-            {"/threads", post_thread_handler, [{objectid, ObjectId}]},
+            {"/threads", post_thread_handler, [
+                {objectid, ObjectId},
+                {mqtt_client, MQTTClient}
+            ]},
             {"/threads/:threadid", thread_handler,[]}
         ]}
     ]),
