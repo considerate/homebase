c7030bce512730813732987fb1ece6b667752ad0
christoffer <cmoc@kth.se>
2015-04-11 21:32:18 +0200

    Check blocked users before inviting

--- src/add_users_to_thread_handler.erl
+++ src/add_users_to_thread_handler.erl
@@ -19,6 +19,15 @@ content_types_accepted(Req, State) ->
 is_authorized(Req, State) ->
     auth_ball:rest_auth(Req,State).
 
+is_blocked(Uid,UsersToAdd) ->
+    {ok,Settings} = application:get_env(homebase,user_api),
+    BasePath = proplists:get_value(base_path,Settings),
+    URL = BasePath ++ "/users/" ++ binary_to_list(Uid),
+    {ok,{_,	_,UserBody}} = httpc:request(URL),
+    {UserData} = jiffy:decode(UserBody),
+    Blocking = proplists:get_value(<<"blocking">>,UserData),
+    lists:any(fun(Blocked) -> sets:is_element(Blocked, UsersToAdd) end, Blocking).
+    
 post_json(Req,Opts) ->
     Thread = cowboy_req:binding(threadid, Req),
     Uid = proplists:get_value(user, Opts),
@@ -26,7 +35,6 @@ post_json(Req,Opts) ->
     {ThreadData} = db_utils:fetch(Thread),
     UsersInThread = proplists:get_value(<<"users">>, ThreadData),
     IsInThread = lists:member(Uid, UsersInThread),
-
     IsPrivate = proplists:get_bool(<<"private">>,ThreadData),
     case IsInThread andalso (not IsPrivate) of
         true ->
@@ -34,19 +42,24 @@ post_json(Req,Opts) ->
             {BodyData} = jiffy:decode(Body),
             UsersToAdd = sets:from_list(proplists:get_value(<<"users">>, BodyData)),
             UsersInThreadSet = sets:from_list(UsersInThread),
-            NewUsers = sets:to_list(sets:union(UsersToAdd, UsersInThreadSet)),
-            NewThreadData = [{<<"users">>, NewUsers}| proplists:delete(<<"users">>, ThreadData)],
-            Output = {[
-                        {users, NewUsers},
-                        {added, sets:to_list(UsersToAdd)},
-                        {id, Thread}
-                        ]},
-            Payload = jiffy:encode(Output),
-            db_utils:put_to_db(Thread, {NewThreadData}),
-            Send = message_utils:send_message(MqttClient,Payload),
-            lists:map(Send, NewUsers),
-            ResultURL = <<"/threads/", Thread/binary>>,
-            {{true,ResultURL}, Req2, Opts};
+            case is_blocked(Uid,UsersToAdd) of
+                false ->  
+                    NewUsers = sets:to_list(sets:union(UsersToAdd, UsersInThreadSet)),
+                    NewThreadData = [{<<"users">>, NewUsers}| proplists:delete(<<"users">>, ThreadData)],
+                    Output = {[
+                                {users, NewUsers},
+                                {added, sets:to_list(UsersToAdd)},
+                                {id, Thread}
+                                ]},
+                    Payload = jiffy:encode(Output),
+                    db_utils:put_to_db(Thread, {NewThreadData}),
+                    Send = message_utils:send_message(MqttClient,Payload),
+                    lists:map(Send, NewUsers),
+                    ResultURL = <<"/threads/", Thread/binary>>,
+                    {{true,ResultURL}, Req2, Opts};
+                true -> 
+                     {false,Req,Opts}
+            end;
         false ->
             {false, Req, Opts}
     end.
