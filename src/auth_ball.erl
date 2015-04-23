a92d9b2e0c4be2d7a48766c1df6f0e2b299adec7
Viktor Kronvall <viktor.kronvall@gmail.com>
2015-04-23 23:45:03 +0200

    Connect with JWT token to be let in.

--- src/auth_ball.erl
+++ src/auth_ball.erl
@@ -8,8 +8,8 @@ secret() ->
 authenticate(Req) ->
     Headers = cowboy_req:headers(Req),
     case proplists:lookup(<<"authorization">>,Headers) of
-        {_, <<"Bearer ", Token/binary>>} -> 
-            case ejwt:parse_jwt(Token, ?SECRET) of
+        {_, <<"Bearer ", Token/binary>>} ->
+            case catch ejwt:parse_jwt(Token, ?SECRET) of
                 {Data} -> {ok, Data};
                 Error -> {error, Error}
             end;
@@ -17,6 +17,7 @@ authenticate(Req) ->
             {error, not_bearer_authorization};
         none ->
             {error, no_authorization_header}
+
     end.
 
 rest_auth(Req, State) ->
